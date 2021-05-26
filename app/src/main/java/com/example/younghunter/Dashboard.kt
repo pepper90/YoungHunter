f076a785b0a808f61bd630169f658ed60dca3d4f
pepper90 <panayotovj@gmail.com>
2021-05-26 07:56:30 +0300

    some minor updates

--- app/src/main/java/com/example/younghunter/Dashboard.kt
+++ app/src/main/java/com/example/younghunter/Dashboard.kt
@@ -1,7 +1,6 @@
 package com.example.younghunter
 
 import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
